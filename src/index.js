782fd563eec99f909639a7ddce82bc0da7a2b9ed
Tatsuya Asami <sharekobekko@gmail.com>
2019-02-10 10:23:13 +0900

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>This is Root</h2>
+      </div>
+    );
+  }
+}
+
+// const Root = () => (
+//   <div>
+//     <h2>This is Root</h2>
+//   </div>
+// );
+
+export default Index;
