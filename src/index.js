22e8f451deb95542ad31e7bee6931601e326e415
Tatsuya Asami <sharekobekko@gmail.com>
2019-02-11 14:25:36 +0900

    add minor change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader'
 import Index from './components/app.js';
-
-// const Index = hot(module)(Index);
 
 ReactDOM.render(
   <Index />,
