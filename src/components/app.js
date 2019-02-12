f5263b4f74ff135861ddfa2c50b52ef86226b096
tatsuya-asami <tatsuya.asami@asia-quest.jp>
2019-02-12 12:22:42 +0900

    add css-loader and create css file

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { hot } from 'react-hot-loader'
 import Test from './test';
 import Counter from './counter';
+import styles from '../index.css'
 
-const Index = () => (
-  <div>
+const Index = (props) => (
+  // <div>
+  <div className="index" style={styles}>
     <Counter />
     <Test />
-    <p>これっっdfs</p>
+    <p>ここはIndex</p>
+    <p>{props.text}</p>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Index from './components/app.js';
 
 ReactDOM.render(
-  <Index />,
+  <Index text="fromIndex" />,
   document.getElementById('root')
 );
